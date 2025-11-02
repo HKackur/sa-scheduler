93cff6829fc78abdcfbbfca8879412f7f08ec8a9
Henrik Kackur <henrik.kackur@sportadmin.se>
2025-11-02 17:07:56 +0100

    Fix: Add Microsoft.EntityFrameworkCore.Infrastructure using for IDbContextFactory

--- SchedulerMVP/Services/ConflictService.cs
+++ SchedulerMVP/Services/ConflictService.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using SchedulerMVP.Data;
 using SchedulerMVP.Data.Entities;
 using SchedulerMVP.Services.Models;
